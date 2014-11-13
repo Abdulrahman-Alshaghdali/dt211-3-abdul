f753e58a3c2836f45fbc2338da480fb4ba51bf75
User <aosk-shg@hotmail.com>
2014-11-13 17:58:47 +0200

    Update listQ 2

--- list-was-queue.py
+++ list-was-queue.py
@@ -9,8 +9,12 @@ from boto.sqs.message import Message
 from boto.sqs.connection import SQSConnection
 from boto.exception import SQSError
 
+conn = boto.sqs.connect_to_region("us-east-1", aws_access_key_id='AKIAIR7EH3TNSTDUCWKA', aws_secret_access_key='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')
 
-conn = boto.sqs.connect_to_region("us-east-1", aws_access_key_id='KIAIR7EH3TNSTDUCWKA', aws_secret_access_key=',t2FZT5mrLYy8gX7kS1q0p4ObQYXTwGnaiUm+rxHZ')
+
+
+q = conn.create_queue("d12123249", 120)
+
 
 rs = conn.get_all_queues()
 for q in rs:
