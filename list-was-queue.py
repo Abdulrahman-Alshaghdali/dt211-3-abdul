1d876f6a9eb9af1117c5bbc92ca47b2522457aba
Abdulrahman Alshaghdali <aosk-shg@hotmail.com>
2014-11-13 15:39:37 +0000

    listq

---
+++ list-was-queue.py
@@ -0,0 +1,17 @@
+# This script created a queue
+#
+# Author - Paul Doyle Aug 2013
+#
+#
+import boto.sqs
+import boto.sqs.queue
+from boto.sqs.message import Message
+from boto.sqs.connection import SQSConnection
+from boto.exception import SQSError
+
+
+conn = boto.sqs.connect_to_region("us-east-1", aws_access_key_id='KIAIR7EH3TNSTDUCWKA', aws_secret_access_key=',t2FZT5mrLYy8gX7kS1q0p4ObQYXTwGnaiUm+rxHZ')
+
+rs = conn.get_all_queues()
+for q in rs:
+	print q.id
